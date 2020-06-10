500e538faa2cab3b55019f629b42c873aa0e3638
oliverlowbob <oliverlowbob@live.dk>
2020-06-10 12:17:16 +0200

    dab

--- seeds/000.delete.js
+++ seeds/000.delete.js
@@ -1,18 +1,11 @@
 
-// exports.seed = function(knex) {
-//   // Deletes ALL existing entries
-//   return knex('users').del()
-//   .then(() =>{
-//     return knex('roles').del()
-
-//   })
-
-// };
-
 exports.seed = function(knex) {
-  return knex('roles').insert([
-    {role: 'ADMIN'},
-    {role: 'MODERATOR'},
-    {role: 'USER'}
-  ]);
+  // Deletes ALL existing entries
+  return knex('users').del()
+  .then(() =>{
+    return knex('roles').del()
+
+  })
+
 };
+
